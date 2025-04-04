e2140022cc236ce22c6e06bcdbfbc4bcf737a565
Roman Di Cesare <romandicesarefer@gmail.com>
2025-04-04 16:56:51 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
